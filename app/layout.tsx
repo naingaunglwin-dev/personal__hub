9f2bc5202de1b3c82d3143f1271c565326dd186f
naingaunglwin-dev <naingaunglwin.wd@gmail.com>
2026-01-27 22:57:39 +0630

    feat: add dotted grid background

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="bg-dots min-h-screen flex items-center justify-center bg-white">
+            {children}
+        </div>
       </body>
     </html>
   );
