88c3cf4acc47b1cc975d2b22589c0cc723483b70
naingaunglwin-dev <naingaunglwin.wd@gmail.com>
2026-01-28 19:59:17 +0630

    feat: implement about page

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+export default function About() {
+  return (
+    <section className="calc-min-h-screen md:calc-min-h-screen-md flex items-center relative overflow-hidden font-anek-devanagari">
+      <span className="hidden lg:block absolute border-l border-gray-200 left-1/4 top-0 bottom-0"></span>
+      <div className="relative w-full">
+        <div className="w-full sm:w-2/3 lg:w-1/2 m-auto">
+          <div className="lg:border-l-4 border-gray-500 ps-4 pt-2 text-4xl">About</div>
+          <div className="text-xl leading-9 lg:leading-12 px-5 py-10 first-letter:text-4xl first-letter:ms-4 text-justify">
+            I'm a passionate developer with experience in PHP and web development, currently working in a Japan-based company, <strong>Aluto-Benli</strong> as a remote developer. I initiated my journey into web development in 2021. I completed a three-month web development fullstack course in April, 2022 and followed by On-Job-Training (OJT) in September, 2022. Currently, I am also developing my own PHP framework and exploring backend development while diving into new technologies. My experience includes working with CodeIgniter, Laravel, and learning Node.js and Next.js. I'm familiar with Git for version control and Composer for package management, with a strong interest in backend development and framework building. I also love writing open-source packages. I also want to learn software development and always curious and eager to explore new technologies.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
