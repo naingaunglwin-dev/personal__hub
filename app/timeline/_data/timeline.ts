5a5e42e82e643c1c5b2a9fbc1e281cc3a5949b1e
naingaunglwin-dev <naingaunglwin.wd@gmail.com>
2026-03-06 00:59:51 +0630

    refactor(timeline): separate page, data, and types into dedicated files

---
+++ app/timeline/_data/timeline.ts
@@ -0,0 +1,20 @@
+import { TimelineProps } from "@/app/timeline/_types/timeline";
+
+export const TimelineData: TimelineProps[] = [
+  {year: "2025, Sep", description: "Joined the 'One Project One Month' program to develop an API project using Laravel"},
+  {year: "2025, End of Aug", description: "Completed the CS50 Python course and received official certificate"},
+  {year: "2025, Aug", description: "Enrolled in the CS50 Python course on edx"},
+  {year: "Start of 2025", description: "Started learning Next.js and developing a personal portfolio website to showcase projects."},
+  {year: "Late 2024-2025", description: "Enhanced a my PHP framework through multiple iterations, improving its architecture and design. Simultaneously built small PHP packages and explored Node.js development"},
+  {year: "2024, Mar", description: "Completed the second version of my php framework"},
+  {year: "2024", description: "Started developing my own php framework second time"},
+  {year: "2023, Dec", description: "Completed the first version of my php framework"},
+  {year: "Late 2023", description: "Began learning how frameworks work"},
+  {year: "2023, Sep", description: "Completed Laravel class"},
+  {year: "2023, Jun", description: "Enrolled in Laravel class at Lobelia Technology"},
+  {year: "2023, May", description: "Started working as a full-time developer at AB Programming Development Team"},
+  {year: "2022, Sep", description: "Received an OJT offer from AB Programming and began learning Codeigniter (PHP Framework) during OJT period"},
+  {year: "2022, Apr", description: "Completed full-stack web development class"},
+  {year: "2022, Jan", description: "Enrolled in a full-stack web development class (PHP) at AB Programming"},
+  {year: "2021", description: "Started exploring web development"},
+];
--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,28 +1,9 @@
 'use client';
 
 import { motion } from "framer-motion";
-import { TimelineProps } from "@/types/timeline";
+import { TimelineData } from "@/app/timeline/_data/timeline";
 
 export default function Timeline() {
-
-  const timeline: TimelineProps[] = [
-    {year: "2025, Sep", description: "Joined the 'One Project One Month' program to develop an API project using Laravel"},
-    {year: "2025, End of Aug", description: "Completed the CS50 Python course and received official certificate"},
-    {year: "2025, Aug", description: "Enrolled in the CS50 Python course on edx"},
-    {year: "Start of 2025", description: "Started learning Next.js and developing a personal portfolio website to showcase projects."},
-    {year: "Late 2024-2025", description: "Enhanced a my PHP framework through multiple iterations, improving its architecture and design. Simultaneously built small PHP packages and explored Node.js development"},
-    {year: "2024, Mar", description: "Completed the second version of my php framework"},
-    {year: "2024", description: "Started developing my own php framework second time"},
-    {year: "2023, Dec", description: "Completed the first version of my php framework"},
-    {year: "Late 2023", description: "Began learning how frameworks work"},
-    {year: "2023, Sep", description: "Completed Laravel class"},
-    {year: "2023, Jun", description: "Enrolled in Laravel class at Lobelia Technology"},
-    {year: "2023, May", description: "Started working as a full-time developer at AB Programming Development Team"},
-    {year: "2022, Sep", description: "Received an OJT offer from AB Programming and began learning Codeigniter (PHP Framework) during OJT period"},
-    {year: "2022, Apr", description: "Completed full-stack web development class"},
-    {year: "2022, Jan", description: "Enrolled in a full-stack web development class (PHP) at AB Programming"},
-    {year: "2021", description: "Started exploring web development"},
-  ];
 
   return (
     <section className="calc-min-h-screen md:calc-min-h-screen-md flex items-center relative overflow-hidden font-anek-devanagari">
@@ -32,7 +13,7 @@ export default function Timeline() {
           <div className="lg:border-l-4 border-gray-500 ps-4 pt-2 text-4xl">#Timeline</div>
              <div className="max-w-4xl m-auto mb-5 space-y-10">
                <ol className="relative space-y-16 before:absolute before:top-0 before:left-1/2 before:h-full before:w-0.5 before:-translate-x-1/2 before:rounded-full before:bg-gray-200 dark:before:bg-gray-700">
-                 {timeline.map((tm, index) => (
+                 {TimelineData.map((tm, index) => (
                    <motion.li
                      key={index}
                      className="group relative grid grid-cols-2 odd:-me-3 even:-ms-3"
